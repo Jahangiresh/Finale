a7136ce389693a1e1566163ff51025f92afb3aae
jahangir <98384389+Jahangiresh@users.noreply.github.com>
2022-03-09 16:49:14 +0400

    FINALE FINALE

---
+++ ConsoleAppFinal/ConsoleAppFinal/interface/IServiceCode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFinal
+
+{
+     interface IServiceCode
+    {
+        public static List<Group> Groups { get; }
+        public static List<Student> Students { get; }
+        public string CreateGroup(Category categories, string no,bool isOnline);
+        public void ShowGroups();
+        public void EditGroupName(string no, string newNo);
+        public void GetGroupStudents(string no);
+        public void GetAllStudents();
+        public void CreateStudent(string fullname,string groupno,bool hasgaranty);
+
+
+    }
+}
---
+++ ConsoleAppFinal/ConsoleAppFinal/models/Group.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppFinal
+{
+    class Group
+    {
+        public string No;
+        public Category Category;
+        public bool IsOnline;
+        public List<Student> Students=new List<Student>();
+        public int Limit;
+
+    
+        public Group(Category category, string no, bool isOnline)
+        {
+            No = no;
+            List<Student> Students = new List<Student>();
+            switch (category)
+            {
+                case Category.Programming:
+                    Console.WriteLine("you chose programming");
+                    break;
+                case Category.Design:
+                    Console.WriteLine("you chose design");
+
+                    break;
+                case Category.System_Administration:
+                    Console.WriteLine("you chose system");
+
+                    break;
+                default:
+                    break;
+
+            }
+            Category = category;
+            IsOnline = isOnline;
+
+
+
+
+
+        }
+        public override string ToString()
+        {
+            string status = IsOnline ? "online qrup" : "offline qrup"; 
+            return $"no:{No}, Category:{Category}, Status: {status}";
+        }
+    }
+
+}
